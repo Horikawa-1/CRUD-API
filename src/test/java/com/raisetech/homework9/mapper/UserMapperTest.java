e627e4527aad8f64ba0050696e3a0fd606b14d36
kaidou2 <rooftop@ymail.ne.jp>
2023-04-18 00:53:45 +0900

    UserMapperTestクラスを一通り完成させた。でも更新処理、削除処理の時に存在しないIDを指定した場合のテストコードは書いていない。後、更新系の処理のDBテストで使うymlファイルを追加して、users.ymlファイルのコードも空白行を入れてちょっと整理した。

--- src/test/java/com/raisetech/homework9/mapper/UserMapperTest.java
+++ src/test/java/com/raisetech/homework9/mapper/UserMapperTest.java
@@ -1,16 +1,20 @@
 package com.raisetech.homework9.mapper;
 
-import static org.hamcrest.MatcherAssert.assertThat;
 
 import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.api.dataset.ExpectedDataSet;
 import com.github.database.rider.spring.api.DBRider;
 import com.raisetech.homework9.entity.User;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+
+import java.util.List;
+import java.util.Optional;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DBRider
 @MybatisTest
@@ -18,13 +22,12 @@ import org.springframework.transaction.annotation.Transactional;
 class UserMapperTest {
 
   @Autowired
-  UserMapper nameMapper;
+  UserMapper userMapper;
 
   @Test
   @DataSet(value = "datasets/users.yml")
-  @Transactional
   void すべてのユーザーが取得できること() {
-    List<User> users = nameMapper.findAll();
+    List<User> users = userMapper.findAll();
     assertThat(users)
         .hasSize(3)
         .contains(
@@ -32,5 +35,50 @@ class UserMapperTest {
             new User(2, "Nakashima"),
             new User(3, "Itou")
         );
+  }
+
+  @Test
+  @DataSet(value = "datasets/empty.yml")
+  void ユーザーが存在しない場合に空のListが取得できること() {
+    List<User> users = userMapper.findAll();
+    assertThat(users).isEmpty();
+  }
+
+  @Test
+  @DataSet(value = "datasets/users.yml")
+  void 引数のidに対応したユーザーを取得できること() {
+    Optional<User> users = userMapper.findById(1);
+    assertThat(users).contains(new User(1, "Honma"));
+  }
+
+  @Test
+  @DataSet(value = "datasets/users.yml")
+  void 引数のidに対応したユーザーが存在しない時_空のOptionalを取得すること() {
+    Optional<User> users = userMapper.findById(0);
+    assertThat(users).isEmpty();
+  }
+
+  @Test
+  @DataSet(value = "users.yml")
+  @ExpectedDataSet(value = "expectedAfterInsertUser.yml", ignoreCols = "id")
+  @Transactional
+  void 新規ユーザーが登録できること() {
+    userMapper.insertUser(new User(4, "まさのり"));
+  }
+
+  @Test
+  @DataSet(value = "users.yml")
+  @ExpectedDataSet(value = "datasets/expectedAfterUpdateUser.yml")
+  @Transactional
+  public void 指定されたidのユーザーが存在する時ユーザー情報が正常に更新されること() {
+    userMapper.updateUser(2, "まさのり");
+  }
+
+  @Test
+  @DataSet(value = "users.yml")
+  @ExpectedDataSet(value = "datasets/expectedAfterDeleteAnime.yml")
+  @Transactional
+  public void 指定されたidが存在する時メッセージが削除されること() {
+    userMapper.deleteUser(2);
   }
 }
