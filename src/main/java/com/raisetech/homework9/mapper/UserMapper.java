106aa85398ad902cfb40d0ce043977ef91b8ae99
kaidou2 <rooftop@ymail.ne.jp>
2023-04-22 20:11:18 +0900

    コンテナ内のデータベースのテーブル名をnamesからusersに変更して、他のクラス（UserMapperクラスのメソッドで使ってるSQL文とか）に書かれてるテーブル名namesをusersに変更。

--- src/main/java/com/raisetech/homework9/mapper/UserMapper.java
+++ src/main/java/com/raisetech/homework9/mapper/UserMapper.java
@@ -14,19 +14,19 @@ import org.apache.ibatis.annotations.Update;
 @Mapper    // MyBatisのMapperである⽬印として@Mapperアノテーションを付与する
 public interface UserMapper {     // classではなくinterfaceで定義する
 
-  @Select("SELECT * FROM names")
+  @Select("SELECT * FROM users")
   List<User> findAll();
 
-  @Select("SELECT * FROM names WHERE id = #{id}")
+  @Select("SELECT * FROM users WHERE id = #{id}")
   Optional<User> findById(int id);
 
   @Options(useGeneratedKeys = true, keyColumn = "id")
-  @Insert("INSERT INTO names (name) VALUES (#{name})")
+  @Insert("INSERT INTO users (name) VALUES (#{name})")
   void insertUser(User name);
 
-  @Update("UPDATE names SET name=#{name} WHERE id = #{id}")
+  @Update("UPDATE users SET name=#{name} WHERE id = #{id}")
   void updateUser(int id, String name);
 
-  @Delete("DELETE FROM names WHERE id = #{id}")
+  @Delete("DELETE FROM users WHERE id = #{id}")
   void deleteUser(int id);
 }
--- src/test/resources/sqlannotation/insert-names.sql
+++ src/test/resources/sqlannotation/insert-names.sql
@@ -1,3 +1,3 @@
-INSERT INTO names (id, name) VALUES (1,"Honma");
-INSERT INTO names (id, name) VALUES (2,"Nakashima");
-INSERT INTO names (id, name) VALUES (3,"Itou");
+INSERT INTO users (id, name) VALUES (1,"Honma");
+INSERT INTO users (id, name) VALUES (2,"Nakashima");
+INSERT INTO users (id, name) VALUES (3,"Itou");
